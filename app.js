8a14650b87be6d8fe172e3bc684d0791da80afae
ORest <orestprustayko@gmail.com>
2015-05-15 01:09:35 +0300

    init files

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Comp1 on 14.05.2015.
+ */
+;(function() {
+    var router = Backbone.Router.extend({
+        router: { //'path/:param':'action'
+          '': 'home'
+        },
+        home: function() {
+        //TODO render HTML
+        }
+    })
+
+
+
+})();
