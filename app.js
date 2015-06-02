32de1af14513c1fc9ab2aeeb70adfef51b70dc1c
ORest <orestprustayko@gmail.com>
2015-06-02 23:33:08 +0300

    init files

--- app.js
+++ app.js
@@ -1,16 +1,63 @@
-/**
- * Created by Comp1 on 14.05.2015.
- */
-;(function() {
-    var router = Backbone.Router.extend({
-        router: { //'path/:param':'action'
-          '': 'home'
+(function () {
+    var appleData = [
+        {
+            name: "fuji",
+            url: "img/fuji.jpg"
         },
-        home: function() {
-        //TODO render HTML
+        {
+            name: "gala",
+            url: "img/gala.jpg"
+        }
+    ];
+
+    var router = Backbone.Router.extend({
+        routes: { //'path/:param':'action'
+            '': 'homeRender',
+            'apples/:appleName': 'appleRender'
+        },
+        initialize: function () {
+            //init model
+            var apples = new Apples;
+            apples.reset(appleData);
+            this.homeView = new homeView({collection: apples});
+            this.appleView = new appleView({collection: apples});
+        },
+        homeRender: function () {
+            this.homeView.render();
+        },
+        appleRender: function (appleName) { //appleName same as in route
+            this.appleView.render(appleName);
         }
     })
 
 
+    var homeView = Backbone.View.extend({
+        el: 'body',
+        template: _.template('Data: <%= data %>'),
+        render: function () {
+            this.$el.html(this.template({
+                data: JSON.stringify(this.collection.models)
+            }));
+        }
+    });
 
+    var appleView = Backbone.View.extend({
+        el: 'body',
+        template: _.template(
+            '<figure>\
+                <a href="<%= attributes.url %>"><%= attributes.name %></a>\
+            </figure>'
+        ),
+        render: function (appleName) {
+            var appleModel = this.collection.where({name: appleName})[0];
+            this.$el.html(this.template(appleModel));
+        }
+    });
+
+
+    var Apples = Backbone.Collection.extend({});
+
+    var app = new router;
+    Backbone.history.start();
 })();
+
